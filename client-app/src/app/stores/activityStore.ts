b93bec2e574a203364513b685b3cdbdc31e9e7cb
Ahmad Rouzbehani <am_rooz@yahoo.com>
2021-12-16 21:09:02 +0330

    end of styling

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -17,6 +17,17 @@ export default class ActivityStore {
             (a, b) => Date.parse(a.date) - Date.parse(b.date)
         );
     }
+    get groupedActivities() {
+        return Object.entries(
+            this.activitiesByDate.reduce((activities, activity) => {
+                const date = activity.date;
+                activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+                return activities;
+
+            }, {} as { [key: string]: Activity[] })
+
+        )
+    }
     loadinActivities = async () => {
         this.loadInitial = true;
         try {
--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,25 +3,26 @@ import React, { useEffect } from "react";
 import { Grid, GridColumn } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingCompoent";
 import { useStore } from "../../../app/stores/store";
+import ActivityFilters from "./ActivityFilters";
 import ActivityList from "./ActivityList";
 
 
 export default observer(function ActivityDashboard() {
     const { activityStore } = useStore();
-    const {loadinActivities,activityRegistry}=activityStore;
+    const { loadinActivities, activityRegistry } = activityStore;
     useEffect(() => {
-      if(activityRegistry.size<=1) loadinActivities();
+        if (activityRegistry.size <= 1) loadinActivities();
 
-    }, [activityRegistry.size,  loadinActivities])
+    }, [activityRegistry.size, loadinActivities])
     if (activityStore.loadInitial) return <LoadingComponent inverted={true} content={'loading app ...'} />
 
     return (
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList />
+                <ActivityList  />
             </Grid.Column>
             <GridColumn width='6'>
-                <h2>Filter</h2>
+                <ActivityFilters />
             </GridColumn>
         </Grid>
     )
